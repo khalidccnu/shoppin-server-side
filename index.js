d7851c51b64fda43dcae919db09e7fa801b8c308
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2023-05-08 13:46:55 +0600

    mongodb connected

--- index.js
+++ index.js
@@ -1,10 +1,43 @@
+const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+dotenv.config();
 app.use(cors());
+
+const mdbClient = new MongoClient(process.env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+(async (_) => {
+  try {
+    await mdbClient.connect();
+    const products = mdbClient.db("shoppin").collection("products");
+
+    app.get("/products", async (req, res) => {
+      const cursor = products.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    mdbClient
+      .db("admin")
+      .command({ ping: 1 })
+      .then((_) => console.log("Successfully connected to MongoDB!"));
+  } catch (err) {
+    console.log("Did not connect to MongoDB! " + err.message);
+  } finally {
+    await mdbClient.close();
+  }
+})();
 
 app.get("/", (req, res) => {
   res.send("Shoppin is running...");
